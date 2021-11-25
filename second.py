28815a3286b7c6bb72cc8a1b0ad9c56f6d2aab71
ch1llay <ch1llay@yandex.ru>
2021-11-25 21:27:06 +0300

    laba2 is done

--- second.py
+++ second.py
@@ -21,7 +21,6 @@ if len(input_string) == 0:
     print("введена пустая строка")
     exit(0)
 run = True
-global_complete = False
 for i in range(len(input_string)):
     symbol = input_string[i]
     if symbol not in "1234":
@@ -35,11 +34,10 @@ for i in range(len(input_string)):
         run = processing(symbol, {"1": "Z", "3": "B"})
     elif current_state == "Z":
         run = processing(symbol, {"2": "A", "4": "B"})
-        global_complete = True
     if not run:
         print("не подходит, так как нет перехода")
         exit(0)
-if not global_complete:
+if current_state != "Z":
     print("не подходит, так как не достигнуто конечное состояние")
     exit(0)
 
